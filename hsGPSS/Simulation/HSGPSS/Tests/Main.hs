b249527092fd1188463e9533a70ae5adc202e17c
MigOrec <MigOrec@gmail.com>
2013-02-28 14:25:23 +0400

    queue + tests

--- hsGPSS/Simulation/HSGPSS/Tests/Main.hs
+++ hsGPSS/Simulation/HSGPSS/Tests/Main.hs
@@ -4,12 +4,14 @@ import Simulation.HSGPSS.Tests.Blocks.Generate
 import Simulation.HSGPSS.Tests.Blocks.Advance
 import Simulation.HSGPSS.Tests.Blocks.Terminate
 import Simulation.HSGPSS.Tests.Blocks.Transfer
+import Simulation.HSGPSS.Tests.Blocks.Queue
 import Test.HUnit
 
 tests = TestList [ TestLabel "generateTests" generateTests,
                    TestLabel "advanceTests" advanceTests,
                    TestLabel "terminateTests" terminateTests,
-                   TestLabel "transferTests" transferTests
+                   TestLabel "transferTests" transferTests,
+                   TestLabel "queueTests" queueTests
                  ]
 
 main = runTestTT tests
