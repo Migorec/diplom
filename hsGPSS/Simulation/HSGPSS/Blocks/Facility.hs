3a2c09f490d94807c40bbf2d6c6a9c9c7d8d360f
MigOrec <MigOrec@gmail.com>
2013-02-28 17:19:40 +0400

    seize,release,preempt,return + tests

---
+++ hsGPSS/Simulation/HSGPSS/Blocks/Facility.hs
@@ -0,0 +1,112 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Simulation.HSGPSS.Blocks.Facility where
+
+import Simulation.HSGPSS.Blocks
+
+seize :: String -> BlockStateMonad
+seize s = addBlock $ Seize s
+
+release :: String -> BlockStateMonad
+release s = addBlock $ Release s
+
+return' :: String -> BlockStateMonad
+return' s = addBlock $ Return s
+
+data PR = PR
+
+class PreemptClass a where
+    preempt :: a -> BlockStateMonad
+    
+instance PreemptClass String where
+    preempt s = addBlock $ PreemptIR s Nothing Nothing False
+    
+instance PreemptClass (String,PR) where
+    preempt (s,PR) = addBlock $ PreemptPR s Nothing Nothing False
+    
+instance PreemptClass (String,(),Double) where
+    preempt (s,(),d) = addBlock $ PreemptIR s (Just $ round d) Nothing False
+    
+instance PreemptClass (String,(),Int) where
+    preempt (s,(),d) = addBlock $ PreemptIR s (Just d) Nothing False
+
+instance PreemptClass (String,PR,Double) where
+    preempt (s,PR,d) = addBlock $ PreemptPR s (Just $ round d) Nothing False
+    
+instance PreemptClass (String,PR,Int) where
+    preempt (s,PR,d) = addBlock $ PreemptPR s (Just d) Nothing False
+    
+instance PreemptClass (String,(),(),Double) where
+    preempt (s,(),(),p) = addBlock $ PreemptIR s Nothing (Just $ round p) False
+    
+instance PreemptClass (String,(),(),Int) where
+    preempt (s,(),(),p) = addBlock $ PreemptIR s Nothing (Just p) False
+    
+instance PreemptClass (String,PR,(),Double) where
+    preempt (s,PR,(),p) = addBlock $ PreemptPR s Nothing (Just $ round p) False
+    
+instance PreemptClass (String,PR,(),Int) where
+    preempt (s,PR,(),p) = addBlock $ PreemptPR s Nothing (Just p) False
+    
+instance PreemptClass (String,(),Double,Double) where
+    preempt (s,(),d,p) = addBlock $ PreemptIR s (Just $ round d) (Just $ round p) False
+    
+instance PreemptClass (String,(),Int,Double) where
+    preempt (s,(),d,p) = addBlock $ PreemptIR s (Just d) (Just $ round p) False
+    
+instance PreemptClass (String,(),Double,Int) where
+    preempt (s,(),d,p) = addBlock $ PreemptIR s (Just $ round d) (Just p) False
+    
+instance PreemptClass (String,(),Int,Int) where
+    preempt (s,(),d,p) = addBlock $ PreemptIR s (Just d) (Just p) False
+    
+instance PreemptClass (String,PR,Double,Double) where
+    preempt (s,PR,d,p) = addBlock $ PreemptPR s (Just $ round d) (Just $ round p) False
+    
+instance PreemptClass (String,PR,Int,Double) where
+    preempt (s,PR,d,p) = addBlock $ PreemptPR s (Just d) (Just $ round p) False
+    
+instance PreemptClass (String,PR,Double,Int) where
+    preempt (s,PR,d,p) = addBlock $ PreemptPR s (Just $ round d) (Just p) False
+    
+instance PreemptClass (String,PR,Int,Int) where
+    preempt (s,PR,d,p) = addBlock $ PreemptPR s (Just d) (Just p) False
+    
+data RE = RE
+    
+instance PreemptClass (String,(),Double,(),RE) where
+    preempt (s,(),d,(),RE) = addBlock $ PreemptIR s (Just $ round d) Nothing True
+    
+instance PreemptClass (String,(),Int,(),RE) where
+    preempt (s,(),d,(),RE) = addBlock $ PreemptIR s (Just d) Nothing True
+    
+instance PreemptClass (String,PR,Double,(),RE) where
+    preempt (s,PR,d,(),RE) = addBlock $ PreemptPR s (Just $ round d) Nothing True
+    
+instance PreemptClass (String,PR,Int,(),RE) where
+    preempt (s,PR,d,(),RE) = addBlock $ PreemptPR s (Just d) Nothing True
+    
+    
+instance PreemptClass (String,(),Double,Double,RE) where
+    preempt (s,(),d,p,RE) = addBlock $ PreemptIR s (Just $ round d) (Just $ round p) True
+    
+instance PreemptClass (String,(),Double,Int,RE) where
+    preempt (s,(),d,p,RE) = addBlock $ PreemptIR s (Just $ round d) (Just p) True
+    
+instance PreemptClass (String,(),Int,Double,RE) where
+    preempt (s,(),d,p,RE) = addBlock $ PreemptIR s (Just d) (Just $ round p) True
+    
+instance PreemptClass (String,(),Int,Int,RE) where
+    preempt (s,(),d,p,RE) = addBlock $ PreemptIR s (Just d) (Just p) True
+    
+instance PreemptClass (String,PR,Double,Double,RE) where
+    preempt (s,PR,d,p,RE) = addBlock $ PreemptPR s (Just $ round d) (Just $ round p) True
+    
+instance PreemptClass (String,PR,Double,Int,RE) where
+    preempt (s,PR,d,p,RE) = addBlock $ PreemptPR s (Just $ round d) (Just p) True
+    
+instance PreemptClass (String,PR,Int,Double,RE) where
+    preempt (s,PR,d,p,RE) = addBlock $ PreemptPR s (Just d) (Just $ round p) True
+    
+instance PreemptClass (String,PR,Int,Int,RE) where
+    preempt (s,PR,d,p,RE) = addBlock $ PreemptPR s (Just d) (Just p) True
