44f687ad0fb79a422af8e51efc3d0bef8ca9a3f7
miniks_iv_migorec <miniks_iv_migorec@79f96485-305b-4f03-a84b-b5a3d3fbe729>
2013-03-15 05:06:55 +0000

    Generateblock on tuples with tests
    git-svn-id: http://dev.iu7.bmstu.ru/svn/diploma_13_i-miniks@4 79f96485-305b-4f03-a84b-b5a3d3fbe729

--- hsGPSS/Tests/Simulation/HSGPSS/Blocks/Generate.hs
+++ hsGPSS/Tests/Simulation/HSGPSS/Blocks/Generate.hs
@@ -1,51 +1,49 @@
-{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE RebindableSyntax, NoImplicitPrelude #-}
 
-module Tests.Simulation.HSGPSS.Blocks.Generate where
+module Tests.Simulation.HSGPSS.Blocks.Generate (generateTests) where
 
 import Simulation.HSGPSS.Blocks
 import Simulation.HSGPSS.Blocks.Generate
 import Test.HUnit
 import Control.Monad.State
-
-import Prelude hiding (fromInteger)
-
-fromInteger :: Integer -> Double
-fromInteger = realToFrac
+import Simulation.HSGPSS.Prelude
 
 oneArgTest = TestCase (assertEqual "for (runState (generate 1) initState)," 
                                    (round(0), BlockState [GenerateRangeNoLimit 1 0 0 (round 0)] (round 1)) 
-                                   (runState (generate 1 :: BlockStateMonad) initState)
+                                   (runState (generate 1) initState )
                       )
 
-twoArgTest = TestCase (assertEqual "for (runState (generate 1 2) initState)," 
+twoArgTest = TestCase (assertEqual "for (runState (generate (1, 2)) initState)," 
                                    (round(0), BlockState [GenerateRangeNoLimit 1 2 0 (round 0)] (round 1)) 
-                                   (runState (generate 1 2 :: BlockStateMonad) initState)
+                                   (runState (generate (1, 2)) initState)
                       )
 
-threeArgTest = TestCase (assertEqual "for (runState (generate 1 2 3) initState)," 
+threeArgTest = TestCase (assertEqual "for (runState (generate (1, 2, 3)) initState)," 
                                      (round(0), BlockState [GenerateRangeNoLimit 1 2 3 (round 0)] (round 1)) 
-                                     (runState (generate 1 2 3 :: BlockStateMonad) initState)
+                                     (runState (generate (1, 2, 3)) initState)
                         )
 
-fourArgTest = TestCase (assertEqual "for (runState (generate 1 2 3 4) initState)," 
+fourArgTest = TestCase (assertEqual "for (runState (generate (1, 2, 3, 4)) initState)," 
                                    (round(0), BlockState [GenerateRangeGeneral 1 2 3 (round 4) (round 0)] (round 1)) 
-                                   (runState (generate 1 2 3 4:: BlockStateMonad) initState)
+                                   (runState (generate (1, 2, 3, 4)) initState)
                       )
 
-fiveArgNoLimitTest = TestCase (assertEqual "for (runState (generate 1 2 3 () 5) initState)," 
+fiveArgNoLimitTest = TestCase (assertEqual "for (runState (generate (1, 2, 3, (), 5)) initState)," 
                                    (round(0), BlockState [GenerateRangeNoLimit 1 2 3 (round 5)] (round 1)) 
-                                   (runState (generate 1 2 3 () 5 :: BlockStateMonad) initState)
+                                   (runState (generate (1, 2, 3, (), 5)) initState)
                               )
                               
-fiveArgTest = TestCase (assertEqual "for (runState (generate 1 2 3 4 5) initState)," 
+fiveArgTest = TestCase (assertEqual "for (runState (generate (1, 2, 3, 4, 5)) initState)," 
                                    (round(0), BlockState [GenerateRangeGeneral 1 2 3 (round 4) (round 5)] (round 1)) 
-                                   (runState (generate 1 2 3 4 5 :: BlockStateMonad) initState)
+                                   (runState (generate (1, 2, 3, 4, 5)) initState)
                       )
 
 multipleBlocks :: BlockStateMonad
-multipleBlocks = do generate 1 :: BlockStateMonad
-                    generate 1 2 :: BlockStateMonad
-                    generate 1 2 3 
+multipleBlocks = do generate 1 
+                    generate (1, 2) 
+                    generate (1, 2,3) 
+                    
+
 
 multipleBlocksTest = TestCase (assertEqual "for (runState multipleBlocks initState),"
                                            (round(2), BlockState [GenerateRangeNoLimit 1 0 0 (round 0),
@@ -55,11 +53,11 @@ multipleBlocksTest = TestCase (assertEqual "for (runState multipleBlocks initSta
                                            (runState multipleBlocks initState)
                               )
 
-tests = TestList [TestLabel "oneArgTest" oneArgTest,
-                  TestLabel "twoArgTest" twoArgTest,
-                  TestLabel "threeArgTest" threeArgTest,
-                  TestLabel "fourArgTest" fourArgTest,
-                  TestLabel "fiveArgNoLimitTest" fiveArgNoLimitTest,
-                  TestLabel "fiveArgTest" fiveArgTest
-                 ]
+generateTests = TestList [TestLabel "oneArgTest" oneArgTest,
+                          TestLabel "twoArgTest" twoArgTest,
+                          TestLabel "threeArgTest" threeArgTest,
+                          TestLabel "fourArgTest" fourArgTest,
+                          TestLabel "fiveArgNoLimitTest" fiveArgNoLimitTest,
+                          TestLabel "fiveArgTest" fiveArgTest
+                         ]
 
