251673f9920f820bdc4cb248c9fa97f750cd39eb
MigOrec <MigOrec@gmail.com>
2013-05-26 20:20:53 +0400

    findOwner functions and tests

--- hsGPSS/Simulation/HSGPSS/Chains.hs
+++ hsGPSS/Simulation/HSGPSS/Chains.hs
@@ -20,8 +20,31 @@ addPC :: PriorityChain -> Transaction -> PriorityChain
 addPC pc transact = before ++ (transact:after)
     where p = priority transact
           (before, after) = span (\x -> priority x >= p) pc
+
+data IR = IRF Double Transaction | IRC Transaction deriving (Eq, Show)
+
+findFInt :: FEC -> String -> (Maybe (Double, Transaction), FEC)
+findFInt [] _ = (Nothing, [])
+findFInt ((t,transact):fs) f| ownership transact == f = (Just (t,transact),fs)
+                            | otherwise = let (r, fs') = findFInt fs f
+                                          in (r,(t,transact):fs')
+
+findCInt :: CEC -> String -> (Maybe Transaction,CEC)
+findCInt [] _ = (Nothing,[])
+findCInt (t:ts) f | ownership t == f = (Just t, ts)
+                  | otherwise = let (r, ts') = findCInt ts f
+                                in (r,t:ts')
+
+findInt :: FEC -> CEC -> String -> (IR,FEC,CEC)
+findInt fec cec f = 
+    case findFInt fec f of
+     (Just (t,transact), fec') -> (IRF t transact, fec', cec)
+     _ -> case findCInt cec f of
+           (Just transact, cec') -> (IRC transact, fec, cec')
+           _ -> error "facility busy, but who did it?"
           
 type IC = [(Double, Transaction)]
+
 
 addIC :: IC -> (Double, Transaction) -> IC
 addIC ic (t, transact) = before ++ ((t,transact):after)
--- hsGPSS/Simulation/HSGPSS/Tests/Chains.hs
+++ hsGPSS/Simulation/HSGPSS/Tests/Chains.hs
@@ -13,6 +13,9 @@ defTransact = Transaction 0 0 0 Active empty ""
 specTransact :: Transaction
 specTransact = Transaction 1 0 0 Active empty ""
 
+ownerTransact :: Transaction
+ownerTransact = Transaction 1 0 0 Passive empty "foo"
+
 emptyFEC = TestCase (assertEqual "for (addFE [] (1,defTransact))," 
                                    ([(1,defTransact)]) 
                                    (addFE [] (1,defTransact))
@@ -38,9 +41,58 @@ multyAddFE = TestCase (assertEqual "for (addFE [(1,defTransact),(2,defTransact),
                                    (addFE [(1,defTransact),(2,defTransact),(2,defTransact),(3,defTransact)] (2,specTransact))
                            )
                            
+findFIntGood1 = TestCase ( assertEqual "for (findFInt [(1,ownerTransact),(2,defTransact),(3,defTransact)] \"foo\")"
+                           (Just (1,ownerTransact),[(2,defTransact),(3,defTransact)])
+                           (findFInt [(1,ownerTransact),(2,defTransact),(3,defTransact)] "foo")
+                        )
+                        
+findFIntGood2 = TestCase ( assertEqual "for (findFInt [(1,defTransact),(2,ownerTransact),(3,defTransact)] \"foo\")"
+                           (Just (2,ownerTransact),[(1,defTransact),(3,defTransact)])
+                           (findFInt [(1,defTransact),(2,ownerTransact),(3,defTransact)] "foo")
+                        )
+                           
+findFIntGood3 = TestCase ( assertEqual "for (findFInt [(1,defTransact),(2,defTransact),(3,ownerTransact)] \"foo\")"
+                           (Just (3,ownerTransact),[(1,defTransact),(2,defTransact)])
+                           (findFInt [(1,defTransact),(2,defTransact),(3,ownerTransact)] "foo")
+                        )
+                        
+findFIntBad = TestCase ( assertEqual "for (findFInt [(1,defTransact),(2,defTransact),(3,defTransact)] \"foo\")"
+                         (Nothing,[(1,defTransact),(2,defTransact),(3,defTransact)])
+                         (findFInt [(1,defTransact),(2,defTransact),(3,defTransact)] "foo")
+                       )
+                       
+findCIntGood1 = TestCase ( assertEqual "for (findCInt [ownerTransact,defTransact,defTransact] \"foo\")"
+                           (Just ownerTransact,[defTransact,defTransact])
+                           (findCInt [ownerTransact,defTransact,defTransact] "foo")
+                         )
+                         
+findCIntGood2 = TestCase ( assertEqual "for (findCInt [defTransact,ownerTransact,defTransact] \"foo\")"
+                           (Just ownerTransact,[defTransact,defTransact])
+                           (findCInt [defTransact,ownerTransact,defTransact] "foo")
+                         )
+                           
+findCIntGood3 = TestCase ( assertEqual "for (findCInt [defTransact,defTransact,ownerTransact] \"foo\")"
+                           (Just ownerTransact,[defTransact,defTransact])
+                           (findCInt [defTransact,defTransact,ownerTransact] "foo")
+                         )
+                           
+findCIntBad = TestCase ( assertEqual "for (findCInt [defTransact,defTransact,defTransact] \"foo\")"
+                           (Nothing,[defTransact,defTransact,defTransact])
+                           (findCInt [defTransact,defTransact,defTransact] "foo")
+                       )
+                           
 chainsTests = TestList [TestLabel "emptyFEC" emptyFEC,
                         TestLabel "middleAddFE" middleAddFE,
                         TestLabel "nearestAddFE" nearestAddFE,
                         TestLabel "lastAddFE" lastAddFE,
-                        TestLabel "multyAddFE" multyAddFE
+                        TestLabel "multyAddFE" multyAddFE,
+                        TestLabel "findFIntGood1" findFIntGood1,
+                        TestLabel "findFIntGood2" findFIntGood2,
+                        TestLabel "findFIntGood3" findFIntGood3,
+                        TestLabel "findFIntBad" findFIntBad,
+                        TestLabel "findCIntGood1" findCIntGood1,
+                        TestLabel "findCIntGood2" findCIntGood2,
+                        TestLabel "findCIntGood3" findCIntGood3,
+                        TestLabel "findCIntBad" findCIntBad
+                        
                        ]
