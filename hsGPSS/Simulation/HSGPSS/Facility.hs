ba46880f46fa027c016481c253b47a0f791790b8
MigOrec <MigOrec@gmail.com>
2013-05-27 11:51:19 +0400

    isInterrupted field added

--- hsGPSS/Simulation/HSGPSS/Facility.hs
+++ hsGPSS/Simulation/HSGPSS/Facility.hs
@@ -4,6 +4,7 @@ import Simulation.HSGPSS.Chains
 import Simulation.HSGPSS.Transaction
 
 data SFacility = SFacility { isAvailable :: Bool,
+                             isInterrupted :: Bool,
                              captureCount :: Int,
                              captureTime :: Double, --суммарно время, в течении которого устройство было занято
                              lastCaptureTime :: Double, -- момент времени, когда устройство стало занято в последний раз
@@ -14,13 +15,20 @@ data SFacility = SFacility { isAvailable :: Bool,
                            } deriving (Eq, Show)
                            
 initFacility :: SFacility
-initFacility = SFacility True 0 0 0 0 [] [] []
+initFacility = SFacility True False 0 0 0 0 [] [] []
 
 queue :: Transaction -> SFacility -> SFacility
 queue t sf = sf{dc = addPC (dc sf) t}
 
 sInterrupt :: (Maybe Double, Transaction) -> SFacility -> SFacility
 sInterrupt r sf = sf{ic = addIC (ic sf) r}
+
+setInterrupt :: SFacility -> SFacility
+setInterrupt = sf{isInterrupted = True}
+
+usetInterrupt :: SFacility -> SFacility
+usetInterrupt = sf{isInterrupted = False}
+
 
 pend :: Transaction -> SFacility -> SFacility
 pend t sf = sf{pc = addPC (pc sf) t}
